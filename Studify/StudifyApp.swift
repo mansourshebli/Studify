ed634f2876f1fa66988c2b4c2d00222311949d18
Mansour Alshebli <mansour.m.alsheblli@gmail.com>
2023-08-31 17:13:56 +0400

    Initial Commit

---
+++ Studify/StudifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudifyApp.swift
+//  Studify
+//
+//  Created by Mansour Mohammed Alshebli on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct StudifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
