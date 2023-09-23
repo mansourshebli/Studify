a3f13604c2c1ecfb6c75d8414c90cedb59edc4d1
Mansour Alshebli <mansour.m.alsheblli@gmail.com>
2023-09-23 09:22:46 +0400

    Fixing spelling mistake :)

--- Studify/Views/TimerView.swift
+++ Studify/Views/TimerView.swift
@@ -57,7 +57,7 @@ struct TimerView: View {
                 }
                 .padding() // Add padding to the grid
             }
-            .navigationTitle("Techniqus")
+            .navigationTitle("Techniques")
         }
     }
 }
