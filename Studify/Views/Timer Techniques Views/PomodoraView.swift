9da1a3d1f64e2f91d5697234deb9e8e270d6501f
Mansour Alshebli <mansour.m.alsheblli@gmail.com>
2023-09-26 20:14:36 +0400

    Structuring the timer's format

--- Studify/Views/Timer Techniques Views/PomodoraView.swift
+++ Studify/Views/Timer Techniques Views/PomodoraView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct PomodoraView: View {
+    @State private var timer: Timer?
+    @State private var timerDuration = 1500
+    @State private var isTimerOn = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            
+            Text(String(format: "%02d:%02d", timerDuration / 60, timerDuration % 60))
+
+        }
+        
     }
+    func formatTime(_ seconds: Int) -> String {
+        let minutes = seconds / 60
+        let seconds = seconds % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
 }
 
 #Preview {
