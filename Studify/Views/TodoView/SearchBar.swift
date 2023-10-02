97a691841b0802ee8d210c3e0d9cb4d85fe7116c
Abdulla-Saeed <101017918+Abdulla-Saeed@users.noreply.github.com>
2023-10-02 19:37:27 +0400

    I have added a search bar that works perfectly, tried to make a scroll view but it didn't work, I have also added a description comment for each view.

---
+++ Studify/Views/TodoView/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  Studify
+//
+//  Created by Abdulla Saeed Alblooshi on 02/10/2023.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
