d9eb8cbf1d4d04464c27af684f068d37b2bee5a3
Abdulla-Saeed <101017918+Abdulla-Saeed@users.noreply.github.com>
2023-10-07 23:39:31 +0400

    .,

--- Studify/Views/TodoView/Todo+Sample.swift
+++ Studify/Views/TodoView/Todo+Sample.swift
@@ -1,8 +1,13 @@
-//
-//  Todo+Sample.swift
-//  Studify
-//
-//  Created by Abdulla Saeed Alblooshi on 07/10/2023.
-//
-
 import Foundation
+
+extension Todo {
+    static let sampleTodos = [
+        Todo(title: "Feeding pets", subtitle: "12", priority: .red, priorityColor: .red),
+        Todo(title: "Buy groceries", subtitle: "34", priority: .orange, priorityColor: .red),
+        Todo(title: "Finish project", subtitle: "56", priority: .red, priorityColor: .red),
+        Todo(title: "Call mom", subtitle: "78", isCompleted: true, priority: .yellow, priorityColor: .yellow),
+        Todo(title: "Go for a run", subtitle: "90", priority: .yellow, priorityColor: .yellow),
+        Todo(title: "Read a book", subtitle: "12", priority: .yellow, priorityColor: .yellow)
+    ]
+    
+}
