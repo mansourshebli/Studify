4b7bfa45bff29c84bc8ca0a92b185f2629d76c40
Abdulla-Saeed <101017918+Abdulla-Saeed@users.noreply.github.com>
2023-11-05 19:19:29 +0400

    Made a progress bar but the app crashes

---
+++ Studify/Views/Dashboard/DashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift
+//  Studify
+//
+//  Created by Abdulla Saeed Alblooshi on 05/11/2023.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
---
+++ Studify/Views/TodoView/ProgressBar.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressBar.swift
+//  Studify
+//
+//  Created by Abdulla Saeed Alblooshi on 05/11/2023.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressBar()
+}
