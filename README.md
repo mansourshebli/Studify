c6d98b74ec99d78af940fa170ed91671af974fb7
Abdulla-Saeed <101017918+Abdulla-Saeed@users.noreply.github.com>
2023-11-06 19:33:58 +0400

    Removed unnecessary comments from all files.

--- README.md
+++
@@ -1,9 +0,0 @@
-# Studify
-
-@mansourshebli
-@Abdulla-Saeed
-
->💡 App made by Mansour Alshebli & Abdulla Alblooshi
-
-# _⚠️ APP WILL BE AVAILABLE IN APP STORE SOON_
-
--- Studify/Views/TodoView/Todo.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct Todo: Identifiable, Codable {
-    var id = UUID()
-    var title: String
-    var subtitle: String
-    var isCompleted = false
-}
