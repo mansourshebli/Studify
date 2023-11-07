799c403d2195e84fab5c57c469d2dc27e621c441
Mansour Alshebli <mansour.m.alshebli@gmail.com>
2023-11-07 18:52:41 +0400

    Dashboard Update

--- Studify/Views/Dashboard/ContentView.swift
+++ Studify/Views/Dashboard/ContentView.swift
@@ -2,22 +2,9 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @AppStorage("isDarkMode") private var isDarkMode = false
     
     
     var body: some View {
-        VStack {
-            Button {
-                isDarkMode.toggle()
-            } label: {
-                Image(systemName: isDarkMode ? "moon" : "sun.max")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 48, height: 48)
-                    .foregroundColor(isDarkMode ? .white : .black)
-            }
-            .offset(x: 150)
-            .preferredColorScheme(isDarkMode ? .dark : .light)
 
             TabView {
                 DashboardView()
@@ -42,7 +29,6 @@ struct ContentView: View {
             }
         }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- Studify/Views/Dashboard/DashboardView.swift
+++ Studify/Views/Dashboard/DashboardView.swift
@@ -18,24 +18,19 @@ struct DashboardView: View {
                 .frame(width: 300, alignment: .top)
                 .position(x: 200, y: 35)
             
+            
             Button {
                 isDarkMode.toggle()
             } label: {
                 Image(systemName: isDarkMode ? "moon" : "sun.max")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 48, height: 48)
+                    .frame(width: 40, height: 40)
                     .foregroundColor(isDarkMode ? .white : .black)
             }
             .position(x: 340, y: 35)
             .preferredColorScheme(isDarkMode ? .dark : .light)
             
-            Rectangle()
-              .foregroundColor(.clear)
-              .frame(width: 300, height: 150)
-              .background(Color(red: 0.21, green: 0.62, blue: 1))
-              .cornerRadius(20)
-              .shadow(color: Color(red: 0.21, green: 0.62, blue: 1).opacity(0.4), radius: 15, x: 0, y: 10)
         }
         
     }
