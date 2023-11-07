2e99b680ded936b5014aafd004565ff7eeab60cc
Mansour Alshebli <mansour.m.alshebli@gmail.com>
2023-11-07 17:38:02 +0400

    Added Rubik Font, New Image Asset, Update to Dashboard

--- Studify/Views/Dashboard/DashboardView.swift
+++ Studify/Views/Dashboard/DashboardView.swift
@@ -2,36 +2,32 @@ import SwiftUI
 
 struct DashboardView: View {
     var body: some View {
-        NavigationView {
-            ScrollView {
-                HStack{
-                    Text("    Gonna be productive today 😎").font(.title2)
-                    Spacer()
-                    }
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))], spacing: 20) {
-                        RoundedRectangle(cornerRadius: 10)
-                            .frame(height: 200)
-                            .foregroundColor(.blue)
-                            .overlay(
-                                Text("Item")
-                                    .foregroundColor(.white)
-                                    .font(.title)
-                            )
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(height: 200)
-                        .foregroundColor(.blue)
-                        .overlay(
-                            Text("Item")
-                                .foregroundColor(.white)
-                                .font(.title)
-                        )
-                }
-                .padding()
-            }
-            .navigationTitle("Dashboard")
+        ZStack {
+            Image("menu")
+                .frame(width: 32, height: 32)
+                .position(x: 45, y: 35)
+
+            Text("Hi, Mansour 👋 ")
+                .font(
+                    Font.custom("Rubik-Regular", size: 20)
+                        .weight(.bold)
+                )
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color(red: 0.18, green: 0.23, blue: 0.44))
+                .frame(width: 141, alignment: .top)
+                .position(x: 200, y: 35)
+            
+            Rectangle()
+              .foregroundColor(.clear)
+              .frame(width: 300, height: 150)
+              .background(Color(red: 0.21, green: 0.62, blue: 1))
+              .cornerRadius(20)
+              .shadow(color: Color(red: 0.21, green: 0.62, blue: 1).opacity(0.4), radius: 15, x: 0, y: 10)
         }
+        
     }
 }
+
 #Preview {
     DashboardView()
 }
