dd8c1403302bcb4636eef9649ab35d1483a58b9d
Mansour Alshebli <mansour.m.alshebli@gmail.com>
2023-11-10 12:15:48 +0400

    Linked all timer techniques to TimerView()

--- Studify/Studify/Views/TimerView/TimerView.swift
+++ Studify/Studify/Views/TimerView/TimerView.swift
@@ -1,53 +1,54 @@
 import SwiftUI
 
 struct TimerView: View {
-    
-    private let techniquesWithIcons: [(String, String, String, Color)] = [
-        ("Pomodoro", "timer", "25 minutes work, 5 minutes break", Color.blue),
-        ("4-4-4-4 Method", "stopwatch", "4 cycles of 25-min work", Color.orange),
-        ("The 52-17 Method", "clock", "52 min work, 17 min break", Color.red),
-        ("The 50-10 Rule", "hourglass", "50 minutes, 10-minute break", Color.green)
+
+    private let techniquesWithIcons: [(String, String, String, Color, AnyView)] = [
+        ("Pomodoro", "timer", "25 minutes work, 5 minutes break", Color.blue, AnyView(PomodoroView())),
+        ("4-4-4-4 Method", "stopwatch", "4 cycles of 25-min work", Color.orange, AnyView(Method4444TimerView())),
+        ("The 52-17 Method", "clock", "52 min work, 17 min break", Color.red, AnyView(Method5217TimerView())),
+        ("The 50-10 Rule", "hourglass", "50 minutes, 10-minute break", Color.green, AnyView(Method5010TimerView()))
     ]
-    
+
     private let adaptiveColumns = [
         GridItem(.adaptive(minimum: 170))
     ]
-    
+
     var body: some View {
         NavigationView {
             ScrollView {
                 LazyVGrid(columns: adaptiveColumns, spacing: 20) {
-                    ForEach(techniquesWithIcons, id: \.0) { (technique, iconName, description, color) in
-                        ZStack {
-                            // Add a gradient background
-                            LinearGradient(gradient: Gradient(colors: [color, color.opacity(0.8)]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                                .frame(width: 170, height: 170)
-                                .cornerRadius(20) // Rounded corners
-                                .shadow(color: Color.gray.opacity(0.5), radius: 5, x: 0, y: 2) // Shadow effect
-                            
-                            VStack {
-                                Image(systemName: iconName)
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 60))
+                    ForEach(techniquesWithIcons, id: \.0) { (technique, iconName, description, color, destinationView) in
+                        NavigationLink(destination: destinationView) {
+                            ZStack {
+                                LinearGradient(gradient: Gradient(colors: [color, color.opacity(0.8)]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                                    .frame(width: 170, height: 170)
+                                    .cornerRadius(20)
+                                    .shadow(color: Color.gray.opacity(0.5), radius: 5, x: 0, y: 2)
                                 
-                                Text(technique)
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 15, weight: .bold, design: .rounded))
-                                    .padding(10) // Add padding to the text
-                                    .background(Color.black.opacity(0.5)) // Background for text
-                                    .cornerRadius(10) // Rounded corners for text background
-                                
-                                Text(description)
-                                    .foregroundColor(.white)
-                                    .multilineTextAlignment(.center)
-                                    .font(.system(size: 14, weight: .regular, design: .rounded))
-                                    .padding(.horizontal, 10)
-                                    .padding(.bottom, 10)
+                                VStack {
+                                    Image(systemName: iconName)
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 60))
+                                    
+                                    Text(technique)
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 15, weight: .bold, design: .rounded))
+                                        .padding(10)
+                                        .background(Color.black.opacity(0.5))
+                                        .cornerRadius(10)
+                                    
+                                    Text(description)
+                                        .foregroundColor(.white)
+                                        .multilineTextAlignment(.center)
+                                        .font(.system(size: 14, weight: .regular, design: .rounded))
+                                        .padding(.horizontal, 10)
+                                        .padding(.bottom, 10)
+                                }
                             }
                         }
                     }
                 }
-                .padding() // Add padding to the grid
+                .padding()
             }
             .navigationTitle("Techniques")
         }
