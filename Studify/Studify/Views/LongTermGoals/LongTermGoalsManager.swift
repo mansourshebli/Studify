256f37686b2e739690d8976fc0270f6915874472
Mansour Alshebli <mansour.m.alshebli@gmail.com>
2023-11-11 19:19:18 +0400

    Added a completionPercentage computed property to LongTermGoalsManager

--- Studify/Studify/Views/LongTermGoals/LongTermGoalsManager.swift
+++ Studify/Studify/Views/LongTermGoals/LongTermGoalsManager.swift
@@ -8,29 +8,33 @@ class LongTermGoalsManager: ObservableObject {
         }
     }
 
+    var completionPercentage: Double {
+        let completedCount = goals.filter { $0.progress == 1.0 }.count
+        let totalCount = goals.count
+        return totalCount > 0 ? Double(completedCount) / Double(totalCount) : 0.0
+    }
 
     init() {
         load()
     }
-    
+
     func getArchiveURL() -> URL {
         let plistName = "goals.plist"
         let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        
         return documentsDirectory.appendingPathComponent(plistName)
     }
-    
+
     func save() {
         let archiveURL = getArchiveURL()
         let propertyListEncoder = PropertyListEncoder()
         let encodedLongTermGoals = try? propertyListEncoder.encode(goals)
         try? encodedLongTermGoals?.write(to: archiveURL, options: .noFileProtection)
     }
-    
+
     func load() {
         let archiveURL = getArchiveURL()
         let propertyListDecoder = PropertyListDecoder()
-                
+
         if let retrievedLongTermGoalData = try? Data(contentsOf: archiveURL),
             let goalsDecoded = try? propertyListDecoder.decode([LongTermGoal].self, from: retrievedLongTermGoalData) {
             goals = goalsDecoded
